cd3d4832e69fc212d68631fcf7645ac27d2c0c86
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-13 17:24:01 +0700

    Add markup

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,17 @@
+import Form from '../form/form';
+import Logo from '../logo/logo';
+import PackingList from '../packing-list/packing-list';
+import Stats from '../stats/stats';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
