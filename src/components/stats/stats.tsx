c470cf98ec1c63d28ef619a1044ddfcd892b9138
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-14 10:31:14 +0700

    Refactored stats component

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -1,6 +1,14 @@
 import { PackItems } from '../../types/types';
 
 export default function Stats({ items }: { items: PackItems }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list!</em>
+      </footer>
+    );
+  }
+
   const readyItems = items.filter((item) => item.packed).length;
   const readyPerProcent = Math.ceil((readyItems * 100) / items.length) || 0;
   const statusMessage =
